d291ae8ff50003c79c9c2cb1e07c82a1390ce3f6
vickygiovi <vickygiovi@gmail.com>
2025-04-05 10:40:39 -0300

    Change Flex to Grid

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,41 @@
+// State of application
+
 let currentNumber = 1;
+let numberOfWindows = 5;
 let letter = "A";
 
+/*----------*/
+
+const ventanillasContainer = document.querySelector(".ventanillas");
+
+document.addEventListener("DOMContentLoaded", () => {
+
+})
+
 const esLetra = (caracter) => {
-	let ascii = caracter.toUpperCase().charCodeAt(0);
-	return ascii > 64 && ascii < 91;
+    let ascii = caracter.toUpperCase().charCodeAt(0);
+    return ascii > 64 && ascii < 91;
 };
 
+const ventanillas = document.querySelectorAll(".ventanilla");
+
 const inputLetter = document.querySelector("#selectLetterInput")
+const inputNumberWindows = document.querySelector("#numberWindowsInput")
+
 inputLetter.addEventListener("input", (e) => {
     const letterInput = e.target.value.charAt(0).toUpperCase()
     if (letterInput != "" && esLetra(letterInput)) {
         letter = letterInput
     }
-    
+
     const numeroTextoVentanilla = document.querySelectorAll(".numeroTexto");
     numeroTextoVentanilla.forEach((elem) => {
         elem.innerText = letter + elem.innerText.substring(1, 4)
     })
+})
+
+inputNumberWindows.addEventListener("input", (e) => {
+    numberOfWindows = e.target.value;
 })
 
 // const numeroV1 = document.getElementById("v1");
@@ -30,6 +49,12 @@ nextNumberButton.forEach((button) => {
     button.addEventListener("click", (event) => {
         let ventanillaAActualizar = event.target.id;
         const numeroVentanilla = document.getElementById("v" + ventanillaAActualizar);
+        const estadoVentanilla = numeroVentanilla.parentNode;
+        ventanillas.forEach((vent) => {
+            vent.classList.remove("ventanillaActual")
+        })
+        estadoVentanilla.classList.add("ventanillaActual")
+        estadoVentanilla.style.display = "flex"
         if (currentNumber.toString().length === 1) {
             numeroVentanilla.innerText = letter + "00" + currentNumber;
         } else if (currentNumber.toString().length === 2) {
@@ -38,9 +63,15 @@ nextNumberButton.forEach((button) => {
             numeroVentanilla.innerText = letter + currentNumber;
         }
 
-        Speak("Ventanilla " + ventanillaAActualizar + " Número " + numeroVentanilla.innerText);
+        Speak("Ventanilla " + ventanillaAActualizar + " Número " + letter + currentNumber);
+
+        Beep(2000)
 
         currentNumber++;
+
+        if (currentNumber === 1000) {
+            currentNumber = 1;
+        }
     });
 });
 
@@ -50,6 +81,16 @@ function Speak(textToSpeech) {
     utterThis.lang = "es"
 
     window.speechSynthesis.speak(utterThis)
+}
+
+function Beep(frequency) {
+    const context = new AudioContext(); //allows access to webaudioapi
+    oscillator = context.createOscillator(); //creates oscillator
+    oscillator.type = "sine"; //chooses the type of wave
+    oscillator.frequency.value = frequency; //assigning the value of oscPitch to the oscillators frequency value
+    oscillator.connect(context.destination); //sends to output
+    oscillator.start(context.currentTime) //starts the sound at the current time
+    oscillator.stop(context.currentTime + 1);
 }
 
 /*-----*/
