9a85d24666979ae69d92dffba77a0efd8662ae3f
vickygiovi <vickygiovi@gmail.com>
2025-04-14 10:30:48 -0300

    Linted

--- src/js/events.js
+++ src/js/events.js
@@ -1,149 +1,149 @@
-import { InitDOM } from "./init.js";
-import { Validation } from "./validations.js";
+// import { InitDOM } from "./init.js";
+// import { Validation } from "./validations.js";
 
-const start = new InitDOM();
+// const start = new InitDOM();
 
-export class Event {
-    constructor(buttonApplyRange, 
-        rangeFromElem, 
-        rangeFrom, 
-        currentNumber, 
-        rangeTo, 
-        rangeToElem, 
-        toggleLetterOrNumber,
-        windowLetter,
-    ) {
-        this.buttonApplyRange = buttonApplyRange;
-        this.rangeFromElem = rangeFromElem;
-        this.rangeFrom = rangeFrom; 
-        this.currentNumber = currentNumber;
-        this.rangeTo = rangeTo;
-        this.rangeToElem = rangeToElem;
-        this.toggleLetterOrNumber = toggleLetterOrNumber;
-        this.windowLetter = windowLetter;
-    }
+// export class Event {
+//     constructor(buttonApplyRange, 
+//         rangeFromElem, 
+//         rangeFrom, 
+//         currentNumber, 
+//         rangeTo, 
+//         rangeToElem, 
+//         toggleLetterOrNumber,
+//         windowLetter,
+//     ) {
+//         this.buttonApplyRange = buttonApplyRange;
+//         this.rangeFromElem = rangeFromElem;
+//         this.rangeFrom = rangeFrom; 
+//         this.currentNumber = currentNumber;
+//         this.rangeTo = rangeTo;
+//         this.rangeToElem = rangeToElem;
+//         this.toggleLetterOrNumber = toggleLetterOrNumber;
+//         this.windowLetter = windowLetter;
+//     }
 
-    registerbuttonApplyRange() {
-        this.buttonApplyRange.addEventListener("click", () => {
+//     registerbuttonApplyRange() {
+//         this.buttonApplyRange.addEventListener("click", () => {
 
-            const validate = new Validation();
+//             const validate = new Validation();
         
-            let valorFrom = parseInt(this.rangeFromElem.value);
-            if (validate.ParseIntCustom(valorFrom)) {
-                this.rangeFrom = valorFrom;
-                if (this.currentNumber < this.rangeFrom || this.currentNumber > this.rangeTo) {
-                    this.currentNumber = this.rangeFrom;
-                }
-            }
+//             let valorFrom = parseInt(this.rangeFromElem.value);
+//             if (validate.ParseIntCustom(valorFrom)) {
+//                 this.rangeFrom = valorFrom;
+//                 if (this.currentNumber < this.rangeFrom || this.currentNumber > this.rangeTo) {
+//                     this.currentNumber = this.rangeFrom;
+//                 }
+//             }
         
-            let valorTo = parseInt(this.rangeToElem.value);
-            if (validate.ParseIntCustom(valorTo)) {
-                this.rangeTo = valorTo;
-                if (this.currentNumber < this.rangeFrom || this.currentNumber > this.rangeTo) {
-                    this.currentNumber = this.rangeFrom;
-                }
-            }
+//             let valorTo = parseInt(this.rangeToElem.value);
+//             if (validate.ParseIntCustom(valorTo)) {
+//                 this.rangeTo = valorTo;
+//                 if (this.currentNumber < this.rangeFrom || this.currentNumber > this.rangeTo) {
+//                     this.currentNumber = this.rangeFrom;
+//                 }
+//             }
         
-            if (this.rangeFrom >= this.rangeTo) {
-                this.currentNumber = 1;
-                this.rangeFrom = 1;
-                this.rangeTo = 999;
-            }
+//             if (this.rangeFrom >= this.rangeTo) {
+//                 this.currentNumber = 1;
+//                 this.rangeFrom = 1;
+//                 this.rangeTo = 999;
+//             }
         
-        })
-    }
+//         })
+//     }
 
-    registertoggleLetterOrNumber() {
-        this.toggleLetterOrNumber.addEventListener("click", () => {
-            this.windowLetter = !this.windowLetter;
+//     registertoggleLetterOrNumber() {
+//         this.toggleLetterOrNumber.addEventListener("click", () => {
+//             this.windowLetter = !this.windowLetter;
         
-            start.init();
+//             start.init();
         
-        })
-    }
+//         })
+//     }
 
-    registerinputLetter() {
-        inputLetter.addEventListener("input", (e) => {
-            const letterInput = e.target.value.charAt(0).toUpperCase()
-            if (letterInput != "" && esLetra(letterInput)) {
-                letter = letterInput
-            }
+//     registerinputLetter() {
+//         inputLetter.addEventListener("input", (e) => {
+//             const letterInput = e.target.value.charAt(0).toUpperCase()
+//             if (letterInput != "" && esLetra(letterInput)) {
+//                 letter = letterInput
+//             }
         
-            const numeroTextoVentanilla = document.querySelectorAll(".numeroTexto");
-            numeroTextoVentanilla.forEach((elem) => {
-                elem.innerText = letter + elem.innerText.substring(1, 4)
-            })
-        })
-    }
+//             const numeroTextoVentanilla = document.querySelectorAll(".numeroTexto");
+//             numeroTextoVentanilla.forEach((elem) => {
+//                 elem.innerText = letter + elem.innerText.substring(1, 4)
+//             })
+//         })
+//     }
 
-    registerinputNumberWindows() {
-        inputNumberWindows.addEventListener("input", (e) => {
-            const validate = new Validation();
-            let inputNumberWindowsValue = parseInt(e.target.value);
-            if (validate.ValidateNumberWindows(inputNumberWindowsValue)) {
-                numberOfWindows = inputNumberWindowsValue;
-                start.init();
-            }
-        })
-    }
+//     registerinputNumberWindows() {
+//         inputNumberWindows.addEventListener("input", (e) => {
+//             const validate = new Validation();
+//             let inputNumberWindowsValue = parseInt(e.target.value);
+//             if (validate.ValidateNumberWindows(inputNumberWindowsValue)) {
+//                 numberOfWindows = inputNumberWindowsValue;
+//                 start.init();
+//             }
+//         })
+//     }
 
-    registernextNumberButton() {
-        nextNumberButton.forEach((button) => {
-            button.addEventListener("click", (event) => {
-                let targetId = event.target.id;
-                let ventanillaAActualizar;
+//     registernextNumberButton() {
+//         nextNumberButton.forEach((button) => {
+//             button.addEventListener("click", (event) => {
+//                 let targetId = event.target.id;
+//                 let ventanillaAActualizar;
         
-                if (windowLetter) {
-                    switch (targetId) {
-                        case "1": ventanillaAActualizar = "A"; break;
-                        case "2": ventanillaAActualizar = "B"; break;
-                        case "3": ventanillaAActualizar = "C"; break;
-                        case "4": ventanillaAActualizar = "D"; break;
-                        case "5": ventanillaAActualizar = "E"; break;
-                        case "6": ventanillaAActualizar = "F"; break;
-                        case "7": ventanillaAActualizar = "G"; break;
-                        case "8": ventanillaAActualizar = "H"; break;
-                        case "9": ventanillaAActualizar = "I"; break;
-                        case "10": ventanillaAActualizar = "J"; break;
-                        default: ventanillaAActualizar = "A";
-                    }
-                } else {
-                    ventanillaAActualizar = targetId;
-                }
+//                 if (windowLetter) {
+//                     switch (targetId) {
+//                         case "1": ventanillaAActualizar = "A"; break;
+//                         case "2": ventanillaAActualizar = "B"; break;
+//                         case "3": ventanillaAActualizar = "C"; break;
+//                         case "4": ventanillaAActualizar = "D"; break;
+//                         case "5": ventanillaAActualizar = "E"; break;
+//                         case "6": ventanillaAActualizar = "F"; break;
+//                         case "7": ventanillaAActualizar = "G"; break;
+//                         case "8": ventanillaAActualizar = "H"; break;
+//                         case "9": ventanillaAActualizar = "I"; break;
+//                         case "10": ventanillaAActualizar = "J"; break;
+//                         default: ventanillaAActualizar = "A";
+//                     }
+//                 } else {
+//                     ventanillaAActualizar = targetId;
+//                 }
         
-                const numeroVentanilla = document.getElementById("v" + targetId);
-                const estadoVentanilla = numeroVentanilla.parentNode;
-                ventanillas.forEach((vent) => {
-                    vent.classList.remove("ventanillaActual")
-                })
-                estadoVentanilla.classList.add("ventanillaActual")
-                estadoVentanilla.style.display = "flex"
-                if (currentNumber.toString().length === 1) {
-                    numeroVentanilla.innerText = letter + "00" + currentNumber;
-                } else if (currentNumber.toString().length === 2) {
-                    numeroVentanilla.innerText = letter + "0" + currentNumber;
-                } else if (currentNumber.toString().length === 3) {
-                    numeroVentanilla.innerText = letter + currentNumber;
-                }
+//                 const numeroVentanilla = document.getElementById("v" + targetId);
+//                 const estadoVentanilla = numeroVentanilla.parentNode;
+//                 ventanillas.forEach((vent) => {
+//                     vent.classList.remove("ventanillaActual")
+//                 })
+//                 estadoVentanilla.classList.add("ventanillaActual")
+//                 estadoVentanilla.style.display = "flex"
+//                 if (currentNumber.toString().length === 1) {
+//                     numeroVentanilla.innerText = letter + "00" + currentNumber;
+//                 } else if (currentNumber.toString().length === 2) {
+//                     numeroVentanilla.innerText = letter + "0" + currentNumber;
+//                 } else if (currentNumber.toString().length === 3) {
+//                     numeroVentanilla.innerText = letter + currentNumber;
+//                 }
         
-                Speak("Ventanilla " + ventanillaAActualizar + " Número " + letter + currentNumber);
+//                 Speak("Ventanilla " + ventanillaAActualizar + " Número " + letter + currentNumber);
         
-                Beep(2000)
+//                 Beep(2000)
         
-                currentNumber++;
+//                 currentNumber++;
         
-                if (currentNumber === rangeTo + 1) {
-                    currentNumber = rangeFrom;
-                }
-            });
-        });
-    }
+//                 if (currentNumber === rangeTo + 1) {
+//                     currentNumber = rangeFrom;
+//                 }
+//             });
+//         });
+//     }
 
-    init() {
-        this.registerbuttonApplyRange();
-        this.registerinputLetter();
-        this.registerinputNumberWindows();
-        this.registernextNumberButton();
-        this.registertoggleLetterOrNumber();
-    }
-}
+//     init() {
+//         this.registerbuttonApplyRange();
+//         this.registerinputLetter();
+//         this.registerinputNumberWindows();
+//         this.registernextNumberButton();
+//         this.registertoggleLetterOrNumber();
+//     }
+// }
--- src/js/init.js
+++ src/js/init.js
@@ -1,150 +1,150 @@
-export class InitDOM {
-    constructor() {
+// export class InitDOM {
+//     constructor() {
 
-    }
+//     }
 
-    UpdateNumberWindows() {
-        let numberWindows = numberOfWindows;
+//     UpdateNumberWindows() {
+//         let numberWindows = numberOfWindows;
     
-        for (let i = 0; i < numberWindows; i++) {
-            nextNumberButton[i].style.display = "block";     
-        }
+//         for (let i = 0; i < numberWindows; i++) {
+//             nextNumberButton[i].style.display = "block";     
+//         }
     
-        for (let i = numberWindows; i < 10; i++) {
-            ventanillas[i].style.display = "none";
-            nextNumberButton[i].style.display = "none";
-        }
-    }
+//         for (let i = numberWindows; i < 10; i++) {
+//             ventanillas[i].style.display = "none";
+//             nextNumberButton[i].style.display = "none";
+//         }
+//     }
 
-    LoadWindows() {
+//     LoadWindows() {
 
-        if (windowLetter) {
-            ventanillaTexto[0].innerText = "Ventanilla A"
-        } else {
-            ventanillaTexto[0].innerText = "Ventanilla 1"
-        }
+//         if (windowLetter) {
+//             ventanillaTexto[0].innerText = "Ventanilla A"
+//         } else {
+//             ventanillaTexto[0].innerText = "Ventanilla 1"
+//         }
     
-        if (windowLetter) {
-            ventanillaTexto[1].innerText = "Ventanilla B"
-        } else {
-            ventanillaTexto[1].innerText = "Ventanilla 2"
-        }
+//         if (windowLetter) {
+//             ventanillaTexto[1].innerText = "Ventanilla B"
+//         } else {
+//             ventanillaTexto[1].innerText = "Ventanilla 2"
+//         }
     
-        if (windowLetter) {
-            ventanillaTexto[2].innerText = "Ventanilla C"
-        } else {
-            ventanillaTexto[2].innerText = "Ventanilla 3"
-        }
+//         if (windowLetter) {
+//             ventanillaTexto[2].innerText = "Ventanilla C"
+//         } else {
+//             ventanillaTexto[2].innerText = "Ventanilla 3"
+//         }
     
-        if (windowLetter) {
-            ventanillaTexto[3].innerText = "Ventanilla D"
-        } else {
-            ventanillaTexto[3].innerText = "Ventanilla 4"
-        }
+//         if (windowLetter) {
+//             ventanillaTexto[3].innerText = "Ventanilla D"
+//         } else {
+//             ventanillaTexto[3].innerText = "Ventanilla 4"
+//         }
     
-        if (windowLetter) {
-            ventanillaTexto[4].innerText = "Ventanilla E"
-        } else {
-            ventanillaTexto[4].innerText = "Ventanilla 5"
-        }
+//         if (windowLetter) {
+//             ventanillaTexto[4].innerText = "Ventanilla E"
+//         } else {
+//             ventanillaTexto[4].innerText = "Ventanilla 5"
+//         }
     
-        if (windowLetter) {
-            ventanillaTexto[5].innerText = "Ventanilla F"
-        } else {
-            ventanillaTexto[5].innerText = "Ventanilla 6"
-        }
+//         if (windowLetter) {
+//             ventanillaTexto[5].innerText = "Ventanilla F"
+//         } else {
+//             ventanillaTexto[5].innerText = "Ventanilla 6"
+//         }
     
-        if (windowLetter) {
-            ventanillaTexto[6].innerText = "Ventanilla G"
-        } else {
-            ventanillaTexto[6].innerText = "Ventanilla 7"
-        }
+//         if (windowLetter) {
+//             ventanillaTexto[6].innerText = "Ventanilla G"
+//         } else {
+//             ventanillaTexto[6].innerText = "Ventanilla 7"
+//         }
     
-        if (windowLetter) {
-            ventanillaTexto[7].innerText = "Ventanilla H"
-        } else {
-            ventanillaTexto[7].innerText = "Ventanilla 8"
-        }
+//         if (windowLetter) {
+//             ventanillaTexto[7].innerText = "Ventanilla H"
+//         } else {
+//             ventanillaTexto[7].innerText = "Ventanilla 8"
+//         }
     
-        if (windowLetter) {
-            ventanillaTexto[8].innerText = "Ventanilla I"
-        } else {
-            ventanillaTexto[8].innerText = "Ventanilla 9"
-        }
+//         if (windowLetter) {
+//             ventanillaTexto[8].innerText = "Ventanilla I"
+//         } else {
+//             ventanillaTexto[8].innerText = "Ventanilla 9"
+//         }
     
-        if (windowLetter) {
-            ventanillaTexto[9].innerText = "Ventanilla J"
-        } else {
-            ventanillaTexto[9].innerText = "Ventanilla 10"
-        }
-    }
+//         if (windowLetter) {
+//             ventanillaTexto[9].innerText = "Ventanilla J"
+//         } else {
+//             ventanillaTexto[9].innerText = "Ventanilla 10"
+//         }
+//     }
 
-    LoadButtons() {
+//     LoadButtons() {
 
-        if (windowLetter) {
-            nextNumberButton[0].innerText = "Next Number Ventanilla A"
-        } else {
-            nextNumberButton[0].innerText = "Next Number Ventanilla 1"
-        }
+//         if (windowLetter) {
+//             nextNumberButton[0].innerText = "Next Number Ventanilla A"
+//         } else {
+//             nextNumberButton[0].innerText = "Next Number Ventanilla 1"
+//         }
     
-        if (windowLetter) {
-            nextNumberButton[1].innerText = "Next Number Ventanilla B"
-        } else {
-            nextNumberButton[1].innerText = "Next Number Ventanilla 2"
-        }
+//         if (windowLetter) {
+//             nextNumberButton[1].innerText = "Next Number Ventanilla B"
+//         } else {
+//             nextNumberButton[1].innerText = "Next Number Ventanilla 2"
+//         }
     
-        if (windowLetter) {
-            nextNumberButton[2].innerText = "Next Number Ventanilla C"
-        } else {
-            nextNumberButton[2].innerText = "Next Number Ventanilla 3"
-        }
+//         if (windowLetter) {
+//             nextNumberButton[2].innerText = "Next Number Ventanilla C"
+//         } else {
+//             nextNumberButton[2].innerText = "Next Number Ventanilla 3"
+//         }
     
-        if (windowLetter) {
-            nextNumberButton[3].innerText = "Next Number Ventanilla D"
-        } else {
-            nextNumberButton[3].innerText = "Next Number Ventanilla 4"
-        }
+//         if (windowLetter) {
+//             nextNumberButton[3].innerText = "Next Number Ventanilla D"
+//         } else {
+//             nextNumberButton[3].innerText = "Next Number Ventanilla 4"
+//         }
     
-        if (windowLetter) {
-            nextNumberButton[4].innerText = "Next Number Ventanilla E"
-        } else {
-            nextNumberButton[4].innerText = "Next Number Ventanilla 5"
-        }
+//         if (windowLetter) {
+//             nextNumberButton[4].innerText = "Next Number Ventanilla E"
+//         } else {
+//             nextNumberButton[4].innerText = "Next Number Ventanilla 5"
+//         }
     
-        if (windowLetter) {
-            nextNumberButton[5].innerText = "Next Number Ventanilla F"
-        } else {
-            nextNumberButton[5].innerText = "Next Number Ventanilla 6"
-        }
+//         if (windowLetter) {
+//             nextNumberButton[5].innerText = "Next Number Ventanilla F"
+//         } else {
+//             nextNumberButton[5].innerText = "Next Number Ventanilla 6"
+//         }
     
-        if (windowLetter) {
-            nextNumberButton[6].innerText = "Next Number Ventanilla G"
-        } else {
-            nextNumberButton[6].innerText = "Next Number Ventanilla 7"
-        }
+//         if (windowLetter) {
+//             nextNumberButton[6].innerText = "Next Number Ventanilla G"
+//         } else {
+//             nextNumberButton[6].innerText = "Next Number Ventanilla 7"
+//         }
     
-        if (windowLetter) {
-            nextNumberButton[7].innerText = "Next Number Ventanilla H"
-        } else {
-            nextNumberButton[7].innerText = "Next Number Ventanilla 8"
-        }
+//         if (windowLetter) {
+//             nextNumberButton[7].innerText = "Next Number Ventanilla H"
+//         } else {
+//             nextNumberButton[7].innerText = "Next Number Ventanilla 8"
+//         }
     
-        if (windowLetter) {
-            nextNumberButton[8].innerText = "Next Number Ventanilla I"
-        } else {
-            nextNumberButton[8].innerText = "Next Number Ventanilla 9"
-        }
+//         if (windowLetter) {
+//             nextNumberButton[8].innerText = "Next Number Ventanilla I"
+//         } else {
+//             nextNumberButton[8].innerText = "Next Number Ventanilla 9"
+//         }
     
-        if (windowLetter) {
-            nextNumberButton[9].innerText = "Next Number Ventanilla J"
-        } else {
-            nextNumberButton[9].innerText = "Next Number Ventanilla 10"
-        }
-    }
+//         if (windowLetter) {
+//             nextNumberButton[9].innerText = "Next Number Ventanilla J"
+//         } else {
+//             nextNumberButton[9].innerText = "Next Number Ventanilla 10"
+//         }
+//     }
 
-    init() {
-        this.UpdateNumberWindows();
-        this.LoadButtons();
-        this.LoadWindows();
-    }
-}
+//     init() {
+//         this.UpdateNumberWindows();
+//         this.LoadButtons();
+//         this.LoadWindows();
+//     }
+// }
